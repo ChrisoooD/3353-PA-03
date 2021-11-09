ae97fdfd91e3f2ed5bb60dd16c4ea6be39a9ecfa
yunchengzhang1 <yunchengz@smu.edu>
2021-11-09 10:49:06 -0600

    cmake update
    enables graphml readin

--- main.cpp
+++ main.cpp
@@ -4,17 +4,80 @@
 #include <boost/property_map/dynamic_property_map.hpp>
 #include <boost/property_map/property_map.hpp>
 #include <fstream>
-using namespace std;
-
-int main() {
-    std::ifstream inFile;
-    inFile.open("football.graphml", std::ifstream::in);
-
+//using namespace std;
+//
+//struct GraphData { std::string Name; };
+//struct VertexProperty { std::string Name; };
+//struct EdgeProperty { std::string Name; };
+//
+//using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::directedS, VertexProperty, EdgeProperty, GraphData>;
+//
+//Graph ReadGraph(std::ifstream& inFile) {
+//    Graph graph;
+//    boost::dynamic_properties dp(boost::ignore_other_properties);
+//    dp.property("Name", boost::get(&VertexProperty::Name, graph));
+//
+//    boost::read_graphml(inFile, graph, dp);
+//
+//    return graph;
+//}
+//
+//
+//int main() {
+//    std::ifstream inFile;
+//    inFile.open("football.graphml", std::ifstream::in);
+//
 //    typedef boost::adjacency_list<> Graph;
 //    Graph g;
-//
+////
 //    boost::dynamic_properties dp;
-//    boost::read_graphml(inFile, g, dp);
-    cout<<"end"<<endl;
+//    g = ReadGraph (inFile);
+//    cout<<"end"<<endl;
+//}
+
+struct GraphData { std::string Name; };
+struct VertexProperty { std::string Name; };
+struct EdgeProperty { std::string Name; };
+
+using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::directedS, VertexProperty, EdgeProperty, GraphData>;
+
+Graph ReadGraph(std::istream& is) {
+    Graph graph;
+    boost::dynamic_properties dp(boost::ignore_other_properties);
+    dp.property("Name", boost::get(&VertexProperty::Name, graph));
+
+    boost::read_graphml(is, graph, dp);
+
+    return graph;
 }
 
+extern std::string const graphml_111;
+
+int main() {
+    std::istringstream is(graphml_111);
+    Graph g = ReadGraph(is);
+    print_graph(g, get(&VertexProperty::Name, g));
+}
+
+std::string const graphml_111 = R"(<?xml version="1.0" encoding="UTF-8"?>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+  <key id="key0" for="node" attr.name="Name" attr.type="string" />
+  <graph id="G" edgedefault="directed" parse.nodeids="canonical" parse.edgeids="canonical" parse.order="nodesfirst">
+    <node id="n0">
+      <data key="key0">A</data>
+    </node>
+    <node id="n1">
+      <data key="key0">D</data>
+    </node>
+    <node id="n2">
+      <data key="key0">B</data>
+    </node>
+    <node id="n3">
+      <data key="key0">C</data>
+    </node>
+    <edge id="e0" source="n0" target="n1">
+    </edge>
+    <edge id="e1" source="n2" target="n3">
+    </edge>
+  </graph>
+</graphml>)";
