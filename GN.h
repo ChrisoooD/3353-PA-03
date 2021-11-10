3405e9a26fdbeca1b5a448be15e07f25471d703b
yunchengzhang1 <yunchengz@smu.edu>
2021-11-10 17:31:23 -0600

    update to GN

--- GN.h
+++ GN.h
@@ -6,16 +6,52 @@
 #include <boost/graph/graphml.hpp>
 #include <boost/property_map/dynamic_property_map.hpp>
 #include <boost/property_map/property_map.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/named_function_params.hpp>
+#include <boost/graph/visitors.hpp>
+#include <boost/array.hpp>
+#include <array>
+#include <utility>
+#include <algorithm>
+#include <fstream>
 #ifndef INC_3353_PA_03_GN_H
 #define INC_3353_PA_03_GN_H
+using namespace std;
+
+struct GraphData { std::string Name; };
+struct VertexProperty { std::string Name; };
+struct EdgeProperty { std::string Name; };
+
+using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS, VertexProperty, EdgeProperty, GraphData>;
 
 class GN{
-private:
-
-
 public:
-    GN(){}
 
+    void gn_run(string filename){
+        std::ifstream inFile(filename);
+        if (! inFile.is_open()){
+            std::cout<<"nope"<<std::endl;
+        }
+        Graph g = ReadIFGraph(inFile);
+        print_graph(g, get(&VertexProperty::Name, g));
+
+        g.m_edges.
+    }
+
+
+    Graph ReadIFGraph(ifstream& is) {
+        Graph graph;
+        boost::dynamic_properties dp(boost::ignore_other_properties);
+        dp.property("Name", boost::get(&VertexProperty::Name, graph));
+
+        boost::read_graphml(is, graph, dp);
+
+        return graph;
+    }
+
+    void calc_edge_betweeness(){
+
+    }
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,45 +1,16 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graph_utility.hpp>
-#include <boost/graph/graphml.hpp>
-#include <boost/property_map/dynamic_property_map.hpp>
-#include <boost/property_map/property_map.hpp>
-#include <boost/graph/breadth_first_search.hpp>
-#include <boost/graph/named_function_params.hpp>
-#include <boost/graph/visitors.hpp>
-#include <boost/array.hpp>
-#include <array>
-#include <utility>
-#include <algorithm>
-#include <fstream>
-
-using namespace std;
 
 
-struct GraphData { std::string Name; };
-struct VertexProperty { std::string Name; };
-struct EdgeProperty { std::string Name; };
+#include "GN.h"
 
-using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS, VertexProperty, EdgeProperty, GraphData>;
 
-Graph ReadIFGraph(std::ifstream& is) {
-    Graph graph;
-    boost::dynamic_properties dp(boost::ignore_other_properties);
-    dp.property("Name", boost::get(&VertexProperty::Name, graph));
 
-    boost::read_graphml(is, graph, dp);
 
-    return graph;
-}
 
 
 int main() {
-    std::ifstream inFile("test.graphml");
-    if (! inFile.is_open()){
-        std::cout<<"nope"<<std::endl;
-    }
-    Graph g = ReadIFGraph(inFile);
-    print_graph(g, get(&VertexProperty::Name, g));
+    GN GirvanN;
 
+    GirvanN.gn_run("test.graphml");
 
 
 }
