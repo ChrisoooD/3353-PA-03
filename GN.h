5af45753a97e4dca49b7f6f6e3b929393f9d84c7
yunchengzhang1 <yunchengz@smu.edu>
2021-11-10 16:47:47 -0600

    gn

---
+++ GN.h
@@ -0,0 +1,27 @@
+//
+// Created by AZ on 11/10/2021.
+//
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/graphml.hpp>
+#include <boost/property_map/dynamic_property_map.hpp>
+#include <boost/property_map/property_map.hpp>
+#ifndef INC_3353_PA_03_GN_H
+#define INC_3353_PA_03_GN_H
+
+class GN{
+private:
+
+
+public:
+    GN(){}
+
+
+
+
+
+};
+
+
+
+#endif //INC_3353_PA_03_GN_H
